bcca8ec895c9de72cfe64fd6319254e614e54533
Thomas Cosse <tomcosse123@gmail.com>
2023-06-12 11:53:20 +0200

    no tile space between tiles

--- app/javascript/controllers/game_controller.js
+++ app/javascript/controllers/game_controller.js
@@ -77,7 +77,7 @@ export default class extends Controller {
 
       // Add tileset to the scene
       const map = this.gameScene.make.tilemap( {key:'dungeon'} )
-      const tileset = map.addTilesetImage('basictiles','tiles')
+      const tileset = map.addTilesetImage('basictiles', 'tiles', 16, 16, 1, 2)
       const groundLayer = map.createLayer('Ground', tileset)
       map.createLayer('Path', tileset)
       const wallsLayer = map.createLayer('Walls', tileset)
@@ -148,7 +148,7 @@ export default class extends Controller {
       autoCenter: Phaser.Scale.CENTER_HORIZONTALLY,
       physics: {
         default: 'arcade',
-        arcade: { debug: false }
+        arcade: { debug: true }
       }
     };
     let game = new Phaser.Game(config);
