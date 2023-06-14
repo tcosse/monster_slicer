9008a942dca793390dcaaac1cdba1abee9fa8415
Thomas Cosse <tomcosse123@gmail.com>
2023-06-14 17:10:14 +0200

    how to play only in game

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_13_125552) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_14_150521) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -38,6 +38,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_13_125552) do
     t.integer "highscore"
     t.integer "total_coins"
     t.integer "total_kills"
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
