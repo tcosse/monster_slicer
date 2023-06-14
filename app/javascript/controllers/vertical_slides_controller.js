25069d0f97e8b40616aab4322be2f998b393909a
Thomas Cosse <tomcosse123@gmail.com>
2023-06-14 13:41:12 +0200

    finished syllabus

--- app/javascript/controllers/vertical_slides_controller.js
+++ app/javascript/controllers/vertical_slides_controller.js
@@ -2,12 +2,15 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="vertical-slides"
 export default class extends Controller {
+  static targets = ['syllabus']
   connect() {
     console.log('Hello from vertical slides controller')
     document.querySelector('body').style.cssText += 'background-color:black';
   }
 
   scrollToSyllabus() {
-    location.hash = '#syllabus'
+    // location.hash = '#syllabus'
+
+    this.syllabusTarget.scrollIntoView()
   }
 }
